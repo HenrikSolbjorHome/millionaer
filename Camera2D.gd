70bd730c77c07cc130d37519de7f3085e4f762e6
HenrikSolbjor <146197599+HenrikSolbjor@users.noreply.github.com>
2025-01-21 11:59:03 +0100

    add code

---
+++ Camera2D.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_button_up():
+	position = Vector2(-8717, 1459.656)
+	set_zoom(Vector2(0.235, 0.13))
--- Main.gd
+++ Main.gd
@@ -1,24 +1,11 @@
 extends Node2D
 
-@onready var brett = $Polygon2D
 
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	var radius = 100  # Radius på sirkelen
-	var segments = 64  # Antall segmenter (jo flere, desto jevnere sirkel)
-	var points = []
-	
-	# Generer punktene for sirkelen
-	for i in range(segments):
-		var angle = (PI * 2 / segments) * i
-		var x = cos(angle) * radius
-		var y = sin(angle) * radius
-		points.append(Vector2(x, y))
-
-	# Definer polygonens punkter
-	brett = points
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	pass
 func _process(delta):
 	pass
+
+
+func _on_player_button_up():
+	pass # Replace with function body.
